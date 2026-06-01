e41cfd856bded173769200200ea20d246fcefe1e
tora29tt-source <tora29tt@gmail.com>
2026-06-01 13:03:16 +0800

    supabase

---
+++ js/supabase-config.js
@@ -0,0 +1,5 @@
+// Supabase 設定
+const SUPABASE_URL = 'https://bbhqvbpsuccbdcnhnobm.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiaHF2YnBzdWNjYmRjbmhub2JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyODQwMzksImV4cCI6MjA5NTg2MDAzOX0.MexR8_hY56m3XRff0EJOQM3uQShXr2L9kGyYXLSzKbs'
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
